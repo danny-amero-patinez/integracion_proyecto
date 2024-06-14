01e0b8e59d96b8306750aa547c7a1f40267559d2
daniel_romero_martinez_unique <daniel.romero@bluestart.mx>
2024-06-13 21:37:41 -0700

    Proyecto

---
+++ src/Configuracion.java
@@ -0,0 +1,8 @@
+package uv.mx.componente_contabilidad;
+
+import org.springframework.boot.SpringBootApplication;
+
+@SpringBootConfiguration
+public class Configuracion {
+  // ... beans y configuraciones para tus tests
+}
---
+++ src/test/java/uv/mx/componente_contabilidad/ComponenteContabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package uv.mx.componente_contabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes=Configuracion.class)
+class ComponenteContabilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
